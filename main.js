65aa2499951d597cb1782eda85030f1615e5af00
mineteeraa <patteera_mine@hotmail.com>
2021-07-01 23:22:06 +0700

    [Add] add details, color, and sizes

--- main.js
+++ main.js
@@ -6,8 +6,16 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             product_link: 'https://www.camt.cmu.ac.th',
             inStock: true,
+            onSale: true,
             inventory: 100,
-            onSale : true
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+                {id: 0001, size: 'S '},
+                {id: 0002, size: 'M '},
+                {id: 0003, size: 'L'}
+            ]
         }
     }
 })
