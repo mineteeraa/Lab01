e406d60b7c92bb7e09ed54b277d1ddf3914cca4c
mineteeraa <patteera_mine@hotmail.com>
2021-07-01 22:26:06 +0700

    [Add] add image and link

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Boots',
-            product_description: 'This is mystery boots that make you can fly in the sky'
+            product_description: 'This is mystery boots that make you can fly in the sky',
+            image: './assets/images/socks_green.jpg',
+            product_link: 'https://www.camt.cmu.ac.th'
         }
     }
 })
