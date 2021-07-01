35863b2386c883538774d5de8f4ccaf2f7768ad0
mineteeraa <patteera_mine@hotmail.com>
2021-07-01 22:01:33 +0700

    [Add] add product description

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',
+            product_description: 'This is mystery boots that make you can fly in the sky'
+        }
+    }
+})
