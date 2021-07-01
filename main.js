1b522ce97abdce815678dd0a5169ac07893458fc
mineteeraa <patteera_mine@hotmail.com>
2021-07-01 22:38:13 +0700

    [Add] add condition inventory and onSale

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Boots',
+            product: 'Shoes',
             product_description: 'This is mystery boots that make you can fly in the sky',
             image: './assets/images/socks_green.jpg',
-            product_link: 'https://www.camt.cmu.ac.th'
+            product_link: 'https://www.camt.cmu.ac.th',
+            inStock: true,
+            inventory: 100,
+            onSale : true
         }
     }
 })
